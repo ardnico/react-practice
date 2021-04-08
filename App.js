a652857d3b0b2deb5d8ce1122ff7685becbc395a
ardnico <leaf.sun2@gmail.com>
2021-04-09 08:24:02 +0900

    20210409_01_commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Text>Add line test</Text>
+      <button onClick={this.handleClick}></button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+function test() {
+  console.log("handling test")
+}
+
+test()
